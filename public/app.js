afbd57246d049ea7a51701b36b8be70cc1c4ca73
jjybaliguat <justinejeraldbaliguat@gmail.com>
2023-02-10 01:58:58 -0800

    Added edit functionality

--- public/app.js
+++ public/app.js
@@ -5,8 +5,12 @@ const formAlert = document.querySelector('.form-alert')
 const alertMessage = document.querySelector('.alert-message')
 const taskInput = document.querySelector('.input-task')
 const deleteBtn = document.querySelector('.delete-btn')
-const taskId = document.querySelector('#taskId')
+const deleteTaskId = document.querySelector('#deleteTaskId')
+const editTaskId = document.querySelector('#editTaskId')
 const deleteSubmit = document.querySelector('#deleteSubmit')
+const editSubmit = document.querySelector('#editSubmit')
+const taskName = document.querySelector('#taskName')
+const taskStatus = document.querySelector('#taskStatus')
 
 // Get all tasks
 const getTasks = async () => {
@@ -15,7 +19,9 @@ const getTasks = async () => {
 
         if(tasks.length < 1){
             noTaskDom.style.display = 'block'
-            noTaskDom.textContent  = 'Task is Empty'
+            noTaskDom.textContent  = 'You have no Task yet'
+            taskDom.innerHTML = ''
+            return
         }
         const allTasks = tasks.map((task) => {
             const { completed, _id: taskID,  name} = task
@@ -23,9 +29,9 @@ const getTasks = async () => {
             return `<div class="single-task ${completed && 'task-completed'}">
             <h5><span><i class="far fa-check-circle"></i></i></span>${name}</h5>
             <div class="task-links">
-                <a href="task.html?id=${taskID}"  class="edit-link text-warning">
-                    <i class="fas fa-edit"></i>
-                </a>
+                <button type="button" class="edit-btn text-warning" data-completed="${completed}" data-name="${name}" data-id="${taskID}" data-toggle="modal" data-target="#editTaskModal">
+                <i class="fas fa-edit"></i>
+                </button>
                 <button type="button" class="delete-btn text-danger" data-id="${taskID}" data-toggle="modal" data-target="#deleteTaskModal">
                     <i class="fas fa-trash"></i>
                 </button>
@@ -34,6 +40,7 @@ const getTasks = async () => {
         }).join('')
 
         noTaskDom.style.display = 'none'
+        noTaskDom.textContent  = ''
         taskDom.innerHTML = allTasks
     } catch (error) {
         console.log(error);
@@ -49,7 +56,7 @@ taskFormDom.addEventListener('submit', async (e) => {
     e.preventDefault()
 
     const name = taskInput.value
-    if(!name.length){
+    if(!name){
         formAlert.style.display = 'block'
         formAlert.textContent = 'Please enter task name'
         formAlert.classList.add('text-danger')
@@ -58,6 +65,11 @@ taskFormDom.addEventListener('submit', async (e) => {
             formAlert.classList.remove('text-success')
             formAlert.classList.remove('text-danger')
           }, 3000)
+        return
+    }else if(name.length > 20){
+        formAlert.style.display = 'block'
+        formAlert.textContent = 'Name cannot be more than 20 characters'
+        formAlert.classList.add('text-danger')
         return
     }
 
@@ -88,19 +100,19 @@ taskDom.addEventListener('click', (e) => {
     deleteSubmit.innerHTML = 'Yes'
     if (el.parentElement.classList.contains('delete-btn')) {
         const id = el.parentElement.dataset.id
-        taskId.value = id
+        deleteTaskId.value = id
     }
 })
 
 deleteSubmit.addEventListener('click', async (e) => {
-    const id = taskId.value
+    const id = deleteTaskId.value
     deleteSubmit.innerHTML = 'Deleting...'
     try {
         await axios.delete(`/api/v1/tasks/${id}`)
-        getTasks()
         alertMessage.style.display = 'block'
         alertMessage.textContent = 'Task Deleted Successfully'
         alertMessage.classList.add('bg-success')
+        getTasks()
     } catch (error) {
         alertMessage.style.display = 'block'
         alertMessage.textContent = 'Failed Deleting task'
@@ -111,3 +123,43 @@ deleteSubmit.addEventListener('click', async (e) => {
         alertMessage.classList.remove('bg-danger')
       }, 3000)
 })
+
+//  edit task
+
+taskDom.addEventListener('click', (e) => {
+    const el = e.target
+    editSubmit.innerHTML = 'Save'
+
+    if (el.parentElement.classList.contains('edit-btn')) {
+        const id = el.parentElement.dataset.id
+        const name = el.parentElement.dataset.name
+        const completed = el.parentElement.dataset.completed
+        editTaskId.value = id
+        taskName.value = name
+        completed === "true" ? taskStatus.checked = true : taskStatus.checked = false
+    }
+})
+
+editSubmit.addEventListener('click', async (e) => {
+    const id = editTaskId.value
+    editSubmit.innerHTML = 'Saving...'
+    try {
+        await axios.patch(`/api/v1/tasks/${id}`, {
+            name: taskName.value,
+            completed: taskStatus.checked
+        })
+        alertMessage.style.display = 'block'
+        alertMessage.textContent = 'Task Updated Successfully'
+        alertMessage.classList.add('bg-success')
+        getTasks()
+    } catch (error) {
+        alertMessage.style.display = 'block'
+        alertMessage.textContent = 'Failed Updating task'
+        alertMessage.classList.add('bg-danger')
+    }setTimeout(() => {
+        alertMessage.style.display = 'none'
+        alertMessage.classList.remove('bg-success')
+        alertMessage.classList.remove('bg-danger')
+      }, 3000)
+})
+
